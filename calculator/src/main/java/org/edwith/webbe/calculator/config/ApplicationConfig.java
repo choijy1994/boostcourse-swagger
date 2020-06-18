4948c04ccd30417e5b377c98d073d26bd1a75895
한컴 <@DESKTOP-L0R9QL8>
2020-06-18 17:46:39 +0900

    boostcourse swagger example first commit

---
+++ calculator/src/main/java/org/edwith/webbe/calculator/config/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package org.edwith.webbe.calculator.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages={"org.edwith.calculator.service"})
+public class ApplicationConfig {
+
+}
